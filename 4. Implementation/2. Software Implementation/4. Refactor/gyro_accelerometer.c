a816e1db8c6dcc79884169d73eb23db0374abdd2
Hugo Carvalho <hugo.mitab@gmail.com>
2020-12-22 17:48:09 +0000

    ADD: _zTasks.h, _zTasks.c, _attr.h

--- 4. Implementation/2. Software Implementation/4. Refactor/gyro_accelerometer.c
+++ 4. Implementation/2. Software Implementation/4. Refactor/gyro_accelerometer.c
@@ -15,25 +15,24 @@
 ***/
 
 #include <math.h>
-#include "mpu6050.h"
+#include "gyro_accelerometer.h"
+#include "i2c.h"
 
-#define RAD_TO_DEG 57.295779513082320876798154814105
+#define M_PI   3.14159265358979323846264338327950288
+#define RAD_TO_DEG(x) (x*180/M_PI)
 
-#define MY_REG 0x75
-#define PWR_MGMT_1_REG 0x6B
-#define SMPLRT_DIV_REG 0x19
-#define ACCEL_CONFIG_REG 0x1C
-#define ACCEL_XOUT_H_REG 0x3B
-//#define TEMP_OUT_H_REG 0x41
-#define GYRO_CONFIG_REG 0x1B
-#define GYRO_XOUT_H_REG 0x43
+#define DEVICE_REG	0x75
+#define PWR_MGMT_1_REG	0x6B
+#define SMPLRT_DIV_REG	0x19
+#define ACCEL_CONFIG_REG	0x1C
+#define ACCEL_XOUT_H_REG	0x3B
+#define GYRO_CONFIG_REG	0x1B
+#define GYRO_XOUT_H_REG	0x43
 
 // Setup MPU6050
 #define MPU6050_ADDR 0xD0
 #define MPU6050_ACK  0x68
-const uint16_t i2c_timeout = 100;
 const double Accel_Z_corrector = 14418.0;
-
 
 uint32_t timer;
 
@@ -49,46 +48,75 @@ Kalman_t KalmanY = {
         .R_measure = 0.03f,
 };
 
+inline double to_degrees(double radians) {
+    return radians * (180.0 / M_PI);
+}
+
+//void XferCpltCallback(DMA_HandleTypeDef* _hdma){
+//	if(_hdma->Instance == DMA1_Stream0)
+//		dma_buffer_flag = 1;
+//}
+
+//void XferCpltCallback(DMA_HandleTypeDef* _hdma) {
+//		if(_hdma->Instance == DMA1_Stream0)
+//			dma_buffer_flag = 1;
+//}
+
 uint8_t MPU6050_Init(I2C_HandleTypeDef* hI2C) {
     uint8_t check;
     uint8_t Data;
 
-    // check device ID WHO_AM_I
-
-    HAL_I2C_Mem_Read_DMA(hI2C, MPU6050_ADDR, MY_REG, 1, &check, 1, i2c_timeout);
+		
+		//HAL_StatusTypeDef state = HAL_DMA_RegisterCallback (hI2C->hdmarx, HAL_DMA_XFER_CPLT_CB_ID, XferCpltCallback);
+	
+		//HAL_DMA_Init(hI2C->hdmarx);
+		//if(i2c_rx_flag)
+		HAL_I2C_Mem_Read_IT(hI2C, MPU6050_ADDR, DEVICE_REG, 1, &check, 1);
+		while (!i2c_rx_flag);
+		i2c_rx_flag = 0;
 
     if (check == MPU6050_ACK)  // 0x68 will be returned by the sensor if everything goes well
     {
         // power management register 0X6B we should write all 0's to wake the sensor up
         Data = 0;
-        HAL_I2C_Mem_Write_DMA(hI2C, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &Data, 1, i2c_timeout);
+        HAL_StatusTypeDef state1 = HAL_I2C_Mem_Write_IT(hI2C, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &Data, 1);
+				while (!i2c_tx_flag);
+				i2c_tx_flag = 0;
 
         // Set DATA RATE of 1KHz by writing SMPLRT_DIV register
         Data = 0x07;
-        HAL_I2C_Mem_Write(hI2C, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, i2c_timeout);
-
+        HAL_StatusTypeDef state2 = HAL_I2C_Mem_Write_IT(hI2C, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1);
+				while (!i2c_tx_flag);
+				i2c_tx_flag = 0;
+			
         // Set accelerometer configuration in ACCEL_CONFIG Register
-        // XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> � 2g
+        // XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0
         Data = 0x00;
-        HAL_I2C_Mem_Write(hI2C, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, i2c_timeout);
-
+        HAL_StatusTypeDef state3 = HAL_I2C_Mem_Write_IT(hI2C, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1);
+				while (!i2c_tx_flag);
+				i2c_tx_flag = 0;
+			
         // Set Gyroscopic configuration in GYRO_CONFIG Register
-        // XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> � 250 �/s
+        // XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0
         Data = 0x00;
-        HAL_I2C_Mem_Write(hI2C, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, i2c_timeout);
-        return 0;
+        HAL_StatusTypeDef state4 = HAL_I2C_Mem_Write_IT(hI2C, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1);
+				while (!i2c_tx_flag);
+				i2c_tx_flag = 0;
+				
+        return 1;
     }
-    return 1;
+    return 0;
 }
 
 
 void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
-    uint8_t Rec_Data[6];
+    uint8_t Rec_Data[6] = {0};
 
     // Read 6 BYTES of data starting from ACCEL_XOUT_H register
-
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, i2c_timeout);
-
+    HAL_StatusTypeDef state = HAL_I2C_Mem_Read_IT(I2Cx, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6);
+		while (!i2c_rx_flag);
+		i2c_rx_flag = 0;
+	
     DataStruct->Accel_X_RAW = (int16_t) (Rec_Data[0] << 8 | Rec_Data[1]);
     DataStruct->Accel_Y_RAW = (int16_t) (Rec_Data[2] << 8 | Rec_Data[3]);
     DataStruct->Accel_Z_RAW = (int16_t) (Rec_Data[4] << 8 | Rec_Data[5]);
@@ -105,11 +133,13 @@ void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
 
 
 void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
-    uint8_t Rec_Data[6];
+    uint8_t Rec_Data[6] = {0};
 
     // Read 6 BYTES of data starting from GYRO_XOUT_H register
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, i2c_timeout);
+    HAL_StatusTypeDef state = HAL_I2C_Mem_Read_IT(I2Cx, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6);
+		while (!i2c_rx_flag);
+		i2c_rx_flag = 0;
 
     DataStruct->Gyro_X_RAW = (int16_t) (Rec_Data[0] << 8 | Rec_Data[1]);
     DataStruct->Gyro_Y_RAW = (int16_t) (Rec_Data[2] << 8 | Rec_Data[3]);
@@ -125,30 +155,18 @@ void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
     DataStruct->Gz = DataStruct->Gyro_Z_RAW / 131.0;
 }
 
-void MPU6050_Read_Temp(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
-    uint8_t Rec_Data[2];
-    int16_t temp;
-
-    // Read 2 BYTES of data starting from TEMP_OUT_H_REG register
-
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, TEMP_OUT_H_REG, 1, Rec_Data, 2, i2c_timeout);
-
-    temp = (int16_t) (Rec_Data[0] << 8 | Rec_Data[1]);
-    DataStruct->Temperature = (float) ((int16_t) temp / (float) 340.0 + (float) 36.53);
-}
-
 void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
-    uint8_t Rec_Data[14];
-    int16_t temp;
-
+    uint8_t Rec_Data[14] = {0};
     // Read 14 BYTES of data starting from ACCEL_XOUT_H register
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 14, i2c_timeout);
+    HAL_StatusTypeDef state = HAL_I2C_Mem_Read_IT(I2Cx, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 14);
+		while (!i2c_rx_flag);
+		i2c_rx_flag = 0;
 
     DataStruct->Accel_X_RAW = (int16_t) (Rec_Data[0] << 8 | Rec_Data[1]);
     DataStruct->Accel_Y_RAW = (int16_t) (Rec_Data[2] << 8 | Rec_Data[3]);
     DataStruct->Accel_Z_RAW = (int16_t) (Rec_Data[4] << 8 | Rec_Data[5]);
-    temp = (int16_t) (Rec_Data[6] << 8 | Rec_Data[7]);
+
     DataStruct->Gyro_X_RAW = (int16_t) (Rec_Data[8] << 8 | Rec_Data[9]);
     DataStruct->Gyro_Y_RAW = (int16_t) (Rec_Data[10] << 8 | Rec_Data[11]);
     DataStruct->Gyro_Z_RAW = (int16_t) (Rec_Data[12] << 8 | Rec_Data[13]);
@@ -156,7 +174,6 @@ void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
     DataStruct->Ax = DataStruct->Accel_X_RAW / 16384.0;
     DataStruct->Ay = DataStruct->Accel_Y_RAW / 16384.0;
     DataStruct->Az = DataStruct->Accel_Z_RAW / Accel_Z_corrector;
-    DataStruct->Temperature = (float) ((int16_t) temp / (float) 340.0 + (float) 36.53);
     DataStruct->Gx = DataStruct->Gyro_X_RAW / 131.0;
     DataStruct->Gy = DataStruct->Gyro_Y_RAW / 131.0;
     DataStruct->Gz = DataStruct->Gyro_Z_RAW / 131.0;
@@ -168,11 +185,11 @@ void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct) {
     double roll_sqrt = sqrt(
             DataStruct->Accel_X_RAW * DataStruct->Accel_X_RAW + DataStruct->Accel_Z_RAW * DataStruct->Accel_Z_RAW);
     if (roll_sqrt != 0.0) {
-        roll = atan(DataStruct->Accel_Y_RAW / roll_sqrt) * RAD_TO_DEG;
+        roll = RAD_TO_DEG(atan(DataStruct->Accel_Y_RAW / roll_sqrt));
     } else {
         roll = 0.0;
     }
-    double pitch = atan2(-DataStruct->Accel_X_RAW, DataStruct->Accel_Z_RAW) * RAD_TO_DEG;
+    double pitch = RAD_TO_DEG(atan2(-DataStruct->Accel_X_RAW, DataStruct->Accel_Z_RAW));
     if ((pitch < -90 && DataStruct->KalmanAngleY > 90) || (pitch > 90 && DataStruct->KalmanAngleY < -90)) {
         KalmanY.angle = pitch;
         DataStruct->KalmanAngleY = pitch;
---
+++ 4. Implementation/2. Software Implementation/5. Additional Files/_zTasks.h
@@ -0,0 +1,7 @@
+/*
+ * _zTasks.h
+ *
+ *  Created on: Dec 22, 2020
+ *      Author: josep & Hugoc
+ */
+
